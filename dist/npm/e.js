768f069906a9f23942e37857d53e3d6ca1ac64ae
王一衡 <760282292@qq.com>
2018-11-14 10:23:53 +0800

    fly修改

--- dist/npm/e.js
+++ dist/npm/e.js
@@ -751,19 +751,19 @@ module.exports = Fly;
  * @Author: leo 
  * @Date: 2018-11-01 16:16:17 
  * @Last Modified by: leo
- * @Last Modified time: 2018-11-01 16:27:40
+ * @Last Modified time: 2018-11-02 16:18:35
  * @description E应用适配器
  */
 module.exports = function (request, responseCallback) {
     var con = {
         method: request.method,
         url: request.url,
-        dataType: request.dataType || undefined,
+        dataType: request.dataType || 'json',
         headers: request.headers,
         data: request.body || {},
         success: function success(res) {
             responseCallback({
-                statusCode: res.statusCode,
+                statusCode: res.status,
                 responseText: res.data,
                 headers: res.header,
                 statusMessage: res.errMsg
@@ -771,7 +771,7 @@ module.exports = function (request, responseCallback) {
         },
         fail: function fail(res) {
             responseCallback({
-                statusCode: res.statusCode || 0,
+                statusCode: res.status || 0,
                 statusMessage: res.errMsg
             });
         }
--- src/adapter/e.js
+++ src/adapter/e.js
@@ -2,19 +2,19 @@
  * @Author: leo 
  * @Date: 2018-11-01 16:16:17 
  * @Last Modified by: leo
- * @Last Modified time: 2018-11-01 16:27:40
+ * @Last Modified time: 2018-11-02 16:18:35
  * @description E应用适配器
  */
 module.exports=function(request, responseCallback) {
   var con = {
       method: request.method,
       url: request.url,
-      dataType: request.dataType || undefined,
+      dataType: request.dataType || 'json',
       headers: request.headers,
       data: request.body||{},
       success(res) {
           responseCallback({
-              statusCode: res.statusCode,
+              statusCode: res.status,
               responseText: res.data,
               headers: res.header,
               statusMessage: res.errMsg
@@ -22,7 +22,7 @@ module.exports=function(request, responseCallback) {
       },
       fail(res) {
           responseCallback({
-              statusCode: res.statusCode||0,
+              statusCode: res.status||0,
               statusMessage: res.errMsg
           })
       }
